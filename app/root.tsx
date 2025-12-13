8bfa2933504e76b2c9b0bcdda9fe3c571726313a
Sam Jonston <johnstonsam712@gmail.com>
2025-12-13 16:13:56 +0000

    add meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -17,6 +17,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Dog Bark Detections</title>
+        <meta name="description" content="View your dogs barks accross time periods" />
         <Meta />
         <Links />
       </head>
